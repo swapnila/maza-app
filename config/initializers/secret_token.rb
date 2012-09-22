23c1861257f358a0a46dd11b1c4e4308765baba4
Ubuntu <home@ubuntu.(none)>
2012-09-22 13:25:24 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MazaApp::Application.config.secret_token = '31065680e85a87922855aa29588c9cf8091c6c63122f4b349d7c582106f124bd011834942f16355fd350bb26929bd0f1c25f7e0ee4a7d71e29fb2d66c24ebd4e'
